fc4faa5a03b5711b8e669af36eb7897885db9e46
Murtaza <murtazab22@gmail.com>
2025-10-07 00:02:01 +0200

    add pokedex website

---
+++ smoljames-tutorial/3_reactjs_full_course/pokedox/src/components/SideNav.jsx
@@ -0,0 +1,47 @@
+import { first151Pokemon, getFullPokedexNumber } from "../utils"
+import { useState } from "react"
+
+export default function SideNav(props){
+    const { selectedPokemon, setSelectedPokemon, handleToggleMenu, showSideMenu } = props
+
+    const [searchValue, setSearchValue] = useState('')
+
+    const filteredPokemon = first151Pokemon.filter((elem, elemIndex) => {
+        // if full pokedex number includes the current search value,
+        // return true
+        if (getFullPokedexNumber(elemIndex).includes(searchValue)){
+            return true
+        }
+        // if the pokemon name includes the current search value,
+        // return true
+        if (elem.toLowerCase().includes(searchValue.toLowerCase())) { return true }
+
+        // otherwise, exclude from the array
+        return false
+    })
+
+    return(
+        <nav className={' ' + (!showSideMenu ? " open" : '')}>
+            <div className={"header " + (!showSideMenu ? " open" : '')}>
+                <button onClick={handleToggleMenu} className="open-nav-button">
+                    <i className="fa-solid fa-arrow-left-long"></i>
+                </button>
+                <h1 className="text-gradient">Pokedex</h1>
+            </div>
+            <input placeholder="E.g. 001 or Bulba.." value={searchValue} onChange={(e) => {
+                setSearchValue(e.target.value)
+            }}/>
+            {filteredPokemon.map((pokemon, pokemonIndex) => {
+                const truePokedexNumber = first151Pokemon.indexOf(pokemon)
+                return(                    
+                    <button onClick={() => {
+                        setSelectedPokemon(truePokedexNumber)
+                    }} key={pokemonIndex} className={'nav-card ' + (pokemonIndex === selectedPokemon ? 'nav-card-selected' : ' ')}>
+                        <p>{getFullPokedexNumber(truePokedexNumber)}</p>
+                        <p>{pokemon}</p>
+                    </button>
+                )
+            })}
+        </nav>
+    )
+}
